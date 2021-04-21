d0afb08f1b60a105ac3f54288a10c4803e82118a
ihatecsv <me@drakeluce.com>
2021-04-21 20:49:42 -0300

    Do FFT, display waterfall

--- index.js
+++ index.js
@@ -5,69 +5,179 @@ const host = "127.0.0.1"; // rtl_tcp server host
 const port = 3000; // rtl_tcp server port
 
 // Radio
-const sampleRate = 250000; // Sample rate in Hz
+const sampleRate = 250000; // Sampling rate in Hz
 const frequency = 104900000; // Frequency in Hz
 const gain = 60; // Gain in dB
+const iqPeakRemoval = false; // Find average of signal and subtract
 
 // Debug
-const recvCountMax = 100; // Max full buffers before exit
-const byteLengthMaxToShow = 32; // Max buffer length to show
 const startupDataDelay = 50; // Delay between sending startup params
-
+const firstPacketLength = 12; // Expected number of bytes for dongle ID
+const averageMax = 1024;
 // END CONFIG
 
 const net = require("net");
+const http = require("http");
+const WebSocket = require("ws");
+const fs = require("fs").promises;
+const transforms = require("@signalprocessing/transforms");
 const client = new net.Socket();
 
-const prefixedBufferFromValue = function(prefix, value, length = 5){
-    if(length <= 0) throw new Error("Cannot use length less than 1!");
-    let position = length - 1;
-    const dataArray = new Array(length).fill(0);
-    dataArray[0] = prefix;
-    while(value != 0){
-        dataArray[position] = value & 0xFF;
-        value >>>= 8;
-        position--;
-        if(position < 0) throw new Error("Data is too large to fit length " + length + "!");
+let bandDataArray = [];
+let currentResolution = 300; // How many samples of the final data should we send
+
+const requestListener = function (req, res) {
+    fs.readFile(__dirname + "/index.html")
+        .then(contents => {
+            res.setHeader("Content-Type", "text/html");
+            res.writeHead(200);
+            res.end(contents);
+        })
+};
+
+const server = http.createServer(requestListener);
+const wss = new WebSocket.Server({ server: server, autoAcceptConnections: false });
+
+const makePrefixedBuffer = function (prefix, value, length = 5) {
+	if (length <= 0) throw new Error("Cannot use length less than 1!");
+	let position = length - 1;
+	const dataArray = new Array(length).fill(0);
+	dataArray[0] = prefix;
+	while (value != 0) {
+		dataArray[position] = value & 0xff;
+		value >>>= 8;
+		position--;
+		if (position < 0)
+			throw new Error("Data is too large to fit length " + length + "!");
+	}
+	return Buffer.from(dataArray);
+};
+
+const dataSum = new Array(sampleRate).fill(0);
+let averageCount = 0;
+
+const fftTransformBandData = async function () {
+    averageCount++;
+	const dataI = [];
+	const dataQ = [];
+    if(averageCount > averageMax){
+        averageCount = Math.floor(averageCount / 2);
+        for(let i = 0; i < dataSum.length; i++){
+            dataSum[i] = Math.floor(dataSum[i] / 2);
+        }
     }
-    return Buffer.from(dataArray);
-}
+	for (let i = 0; i < bandDataArray.length; i++) {
+		if (i % 2 == 0) {
+			dataI.push(bandDataArray[i] - 127.5);
+		} else {
+			dataQ.push(bandDataArray[i] - 127.5);
+		}
+	}
+	const period = 1 / sampleRate;
+	let t = Array.from({ length: dataI.length }, (x, i) => i * period);
+	let [fftOutReal, fftOutComplex] = transforms.fft(
+		dataI,
+		dataQ
+	);
 
-const sampleRateData = prefixedBufferFromValue(0x02, sampleRate);
-const frequencyData = prefixedBufferFromValue(0x01, frequency);
-const rtlAgcModeData = Buffer.from([0x08,0x00,0x00,0x00,0x00]); // RTL AGC mode auto
-const gainModeData = Buffer.from([0x08,0x00,0x00,0x00,0x00]); // Gain mode auto
-const agcModeData = Buffer.from([0x0D,0x00,0x00,0x00,0x00]); // AGC mode auto
-const gainValueData = prefixedBufferFromValue(0x04, gain);
+    const sendDataBuffer = [];
 
-const startupData = [sampleRateData, frequencyData, rtlAgcModeData, gainModeData, agcModeData, gainValueData];
+    for(let i = 0; i < fftOutReal.length; i++){
+        if(i % 2 == 0){
+            dataSum[i] += fftOutReal[i];
+            dataSum[i+1] += fftOutComplex[i];
+
+            fftOutReal[i] = fftOutReal[i] - iqPeakRemoval ? Math.round(dataSum[i] / averageCount) : 0;
+            fftOutComplex[i] = fftOutComplex[i] - iqPeakRemoval ? Math.round(dataSum[i+1] / averageCount) : 0;
+        }
+        const magnitude = Math.round(Math.sqrt((fftOutReal[i] * fftOutReal[i]) + (fftOutComplex[i] * fftOutComplex[i])));
+        if(i % Math.floor(fftOutReal.length / currentResolution) == 0) sendDataBuffer.push(magnitude);
+    }
+
+    let data = Uint16Array.from(sendDataBuffer);
+
+    wss.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(data);
+        }
+    });
+};
+
+const sampleRateData = makePrefixedBuffer(0x02, sampleRate);
+const frequencyData = makePrefixedBuffer(0x01, frequency);
+const rtlAgcModeData = Buffer.from([0x08, 0x00, 0x00, 0x00, 0x00]); // RTL AGC mode auto
+const gainModeData = Buffer.from([0x08, 0x00, 0x00, 0x00, 0x00]); // Gain mode auto
+const agcModeData = Buffer.from([0x0d, 0x00, 0x00, 0x00, 0x00]); // AGC mode auto
+const gainValueData = makePrefixedBuffer(0x04, gain);
+
+const startupData = [
+	sampleRateData,
+	frequencyData,
+	rtlAgcModeData,
+	gainModeData,
+	agcModeData,
+	gainValueData,
+];
 
 let recvCount = 0;
+let firstPacket = true;
 
-const writeStartupData = function(i = 0){
-    client.write(startupData[i]);
-    if(i == startupData.length - 1) return;
-    setTimeout(function(){
-        writeStartupData(i + 1);
-    }, startupDataDelay);
-}
+const writeStartupData = function (i = 0) {
+	client.write(startupData[i]);
+	if (i == startupData.length - 1) return;
+	setTimeout(function () {
+		writeStartupData(i + 1);
+	}, startupDataDelay);
+};
 
-
-client.connect(port, host, function() {
-	console.log("Connected!");
-    writeStartupData();
+client.connect(port, host, function () {
+	console.log("Connected to rtl_tcp server at " + host + ":" + port);
+	writeStartupData();
 });
 
-client.on("data", function(data) {
-    recvCount++;
-    const byteLength = Buffer.byteLength(data);
-	console.log("Received " + byteLength + " bytes!" + (byteLength > byteLengthMaxToShow ? " (Data too long)" : " (" + data.toString("hex") + ")"));
-    if(recvCount >= recvCountMax){
-        console.log("Client destroyed, reached max recv count!");
-        client.destroy();
-    }
+client.on("data", function (data) {
+	if (firstPacket) {
+		if (data.length != firstPacketLength) {
+			throw new Error(
+				"Unexpected number of bytes for first packet! Expected " +
+					firstPacketLength +
+					", got " +
+					data.length +
+					"!"
+			);
+		}
+		console.log(
+			"Parameters set successfully. Dongle ID: " + data.toString("hex")
+		);
+		firstPacket = false;
+		return;
+	}
+	recvCount++;
+	const byteLength = Buffer.byteLength(data);
+	let i = 0;
+	for (; i < byteLength; i++) {
+		if (bandDataArray.length >= sampleRate * 2) break;
+		bandDataArray.push(data[i]);
+	}
+	if (i < byteLength) {
+		fftTransformBandData();
+		bandDataArray = [];
+		for (; i < byteLength; i++) {
+			bandDataArray.push(data[i]);
+		}
+	}
 });
 
-client.on("close", function() {
+client.on("close", function () {
 	console.log("Connection closed");
 });
+
+wss.on("connection", function connection(ws) {
+    console.log("New WS client!");
+	ws.on("message", function incoming(data) {
+        // New resolution
+        currentResolution = parseInt(data);
+	});
+});
+
+server.listen(8080, "127.0.0.1");
